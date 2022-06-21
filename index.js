44ecd92f848acd6eaa60f4134f019aad182f96ea
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-21 12:30:58 +0100

    two methods to do the same thing

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import { parse } from 'csv-parse';
 import * as fs from 'fs';
 
+//var parser = parse({columns: true}, function(err, records){
+    //console.log(records)
+//});
+//fs.createReadStream('./Transactions2014.csv').pipe(parser);
+//let fileContents = fs.readFile('./Transactions2014.csv', 'utf8', (err, data)=>{
+    //console.log(data);
+//});
 
-var parser = parse({columns: true}, function(err, records){
-    console.log(records);
+const data = fs.readFileSync('./Transactions2014.csv', 'UTF-8');
+const lines = data.split(/\r?\n/);
+
+lines.forEach((line) => {
+    console.log(line)
 });
-
-fs.createReadStream('./Transactions2014.csv').pipe(parser);
